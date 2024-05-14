c5bd9118980b816331f51cb6fe218694475b54c4
MakerScript <104077050+MakerScript@users.noreply.github.com>
2024-05-14 15:07:32 +0700

    Delete cleaner directory

--- cleaner/CLR/CLR_APP/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Genre, Language, App, Author
-
-admin.site.register(Genre)
-admin.site.register(Language)
-admin.site.register(App)
-admin.site.register(Author)
-
