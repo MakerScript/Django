48dac97cf61ffa114ac283047e8bec7a46467dab
MakerScript <104077050+MakerScript@users.noreply.github.com>
2024-05-17 15:00:02 +0700

    Delete CLR directory

--- CLR/CLR_APP/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-
-class HomePageView(TemplateView):
-    template_name = 'home.html'
-
-class AboutPageView(TemplateView): # новое
-    template_name = 'about.html'
